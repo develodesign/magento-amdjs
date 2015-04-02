00ce1ead05566e021f8b3d6c6793d11c70dac716
Paul Bill <paul@develodesign.co.uk>
2015-04-02 14:03:25 +0100

    Ordering for modules and exposing addskinjs method

--- code/community/MaxBucknell/AMDJS/Block/Modules.php
+++ code/community/MaxBucknell/AMDJS/Block/Modules.php
@@ -14,27 +14,26 @@ class MaxBucknell_AMDJS_Block_Modules extends Mage_Core_Block_Template
     }
 
     /**
-     * Add a module to this page.
+     * Add a js file.
+     * If an index is defined then add it at the index.
      *
-     * @param string $moduleName
-     * @return void
+     * @param $url string
+     * @param null $position
      */
-    public function addModule ($moduleName)
+    private function _add( $url, $position = null )
     {
-        $this->_modules[$moduleName] = $moduleName;
+        if( ! $position )
+
+            $this->_modules[] = $url;
+
+        else
+
+            $this->_modules[ $position ] = $url;
     }
 
     /**
-     * Stop using a module on this page.
      *
-     * @param string $moduleName
-     * @return void
      */
-    public function removeModule($moduleName)
-    {
-        unset($this->_modules[$moduleName]);
-    }
-
     protected function _beforeToHtml()
     {
         $helper = Mage::helper('MaxBucknell_AMDJS');
@@ -46,5 +45,53 @@ class MaxBucknell_AMDJS_Block_Modules extends Mage_Core_Block_Template
         $this->assign('requireJSUrl', $this->getJsUrl('maxbucknell/amdjs/lib/bower_components/requirejs/require.js'));
         $this->assign('compiledScriptURL', $helper->getBuiltFileUrl($this->_modules));
         $this->assign('modules', array_values($this->_modules));
+    }
+
+    /**
+     * Adds a javascript file and an optional position.
+     * If position is taken it'll save the item at the old position, add the new item, then save the original
+     * item add the end of the array.
+     *
+     * @param $url String
+     * @param $position String|Integer - index
+     */
+    public function addModule( $url, $position = null )
+    {
+        if( ! $position || ! $this->_isPositionTaken( $position ) )
+
+            $this->_add( $url, $position );
+
+        else
+        {
+            // Position is probably taken.
+
+            $originalItem = $this->_modules[ $position ];
+
+            $this->_add( $url, $position );
+
+            $this->_add( $originalItem );
+        }
+    }
+
+    /**
+     * Add a js file from the skin directory of the current theme
+     *
+     * @param $file
+     * @param $position
+     */
+    public function addSkinJs( $file, $position = null )
+    {
+        $this->addModule( $this->getSkinUrl( $file ), $position );
+    }
+
+    /**
+     * Stop using a module on this page.
+     *
+     * @param string $moduleName
+     * @return void
+     */
+    public function removeModule($moduleName)
+    {
+        unset($this->_modules[$moduleName]);
     }
 }
